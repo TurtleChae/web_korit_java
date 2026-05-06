250c94558d5358d8a7724005b5dcd139532bc03a
TurtleChae <turtle@example.com>
2026-05-06 10:28:17 +0900

    Object

--- web_korit_java-master/src/main/java/_19_NestedClass/LombokUser.java
+++ web_korit_java-master/src/main/java/_19_NestedClass/LombokUser.java
@@ -5,10 +5,15 @@ package _19_NestedClass;
 // 2. 메이븐 레파지토리에서 lombok 검색 후 pom.xml에 기입
 // 3. maven 동기화
 
-@Builder // 알아서 컴파일할 때 코드를 추가해줌
-@AllArgsConstructor // 모든 필드를 받아주는 생성자
-@Data // getter, setter, toString, equals, hashCode 작성
+// 알아서 컴파일할 때 코드를 추가해줌
+@Builder
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드 생성자q
+@Data // getter, setter, Object오버라이딩, final 필드 생성자
+@Getter @Setter @ToString @EqualsAndHashCode @RequiredArgsConstructor작성
 public class LombokUser {
-    private String username;
-    private String password;
+    private final String username;
+    private final String password;
+
+
 }
